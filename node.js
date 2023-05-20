d55aea25b61f2a22f291800dfbfba36e777c6048
Sreejita2023 <130359704+Sreejita2023@users.noreply.github.com>
2023-05-20 17:30:56 +0530

    Add files via upload

---
+++ node.js
@@ -0,0 +1,19 @@
+const sounds=['applause','boo','gasp','tada']
+sounds.forEach(sound=>{
+    const btn=document.createElement('button')
+    btn.classList.add('btn')
+    btn.innerText=sound
+    btn.addEventListener('click',()=>{
+        stopsongs()
+        document.getElementById(sound).play()
+    })
+    document.getElementById('buttons').appendChild(btn)  
+
+})
+function stopsongs(){
+    sounds.forEach(sound=>{
+        const song=document.getElementById(sound)
+        song.pause()
+        song.currentTime=0
+    })
+}
